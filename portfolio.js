5bdd2f85077bd56834e3bedea03a49af08a3e945
G Swarnalatha <swarnalathaguggilam084@gmail.com>
2024-12-09 20:46:41 +0530

    added my portfolio

---
+++ portfolio.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetID = this.getAttribute('href');
+            const target = document.querySelector(targetID);
+
+            if (target) {
+                const headerHeight = document.querySelector('header').offsetHeight;
+                const targetPosition = target.offsetTop - headerHeight;
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
+
